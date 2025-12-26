a5b6acb79c9d9ca538673c31f1b923c7c556967c
Mike <jacky14440@gmail.com>
2025-12-26 17:53:14 +0800

    按鈕功能尚未完成

---
+++ project-即將上線頁面/app.js
@@ -0,0 +1,5 @@
+function send() {
+  const input = document.querySelector(".input-style");
+  const button = document.querySelector(".input-box-span");
+  const errorText = document.querySelector(".input-error-text");
+}
