0ec5f35beb8705c8f155600975ed09060f866388
Mike <jacky14440@gmail.com>
2025-12-29 17:23:21 +0800

    1229 第一份有js專案done

--- project-即將上線頁面/app.js
+++ project-即將上線頁面/app.js
@@ -1,5 +1,28 @@
-function send() {
-  const input = document.querySelector(".input-style");
-  const button = document.querySelector(".input-box-span");
-  const errorText = document.querySelector(".input-error-text");
+function isValidEmail(email) {
+  return email.includes("@") && email.includes(".");
 }
+
+function Send() {
+  const input = document.querySelector(".input-style");
+  const errorText = document.querySelector(".input-error-text");
+  const email = input.value.trim();
+  // errorText.style.display = "block";
+  // input.style.border = "1px solid red";
+  // 加!=不是
+  if (!isValidEmail(email)) {
+    errorText.style.display = "block";
+    input.style.border = "1px solid red";
+  } else {
+    errorText.style.display = "none";
+    input.style.border = "1px solid #ccc";
+    alert("Email sent!");
+  }
+}
+const input = document.querySelector(".input-style");
+const errorText = document.querySelector(".input-error-text");
+input.addEventListener("input", () => {
+  if (input.value === "") {
+    errorText.style.display = "none";
+    input.style.border = "1px solid #ccc";
+  }
+});
